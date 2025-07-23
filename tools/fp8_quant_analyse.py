b3b6c34d325127e81f4d1d4eafa0eadb5218638d
woodchen7 <woodchenwu@tencent.com>
2025-07-23 20:09:19 +0800

    standardized

--- tools/fp8_quant_analyse.py
+++ tools/fp8_quant_analyse.py
@@ -18,6 +18,23 @@ from angelslim.compressor.quant.core.fp8_analyse_tools import draw_bf16_fp8_weig
 import os
 
 
+def quant_analyse(args):
+    if args.analyse_type == 'act':
+        os.makedirs(args.save_path, exist_ok=True)
+        assert os.path.exists(args.model_path), f"File {args.model_path} not exist"
+        print(f"[AngelSlim] Save all quant scale graph to {args.save_path}")
+        draw_fp8_scale_fig(args.model_path, args.save_path)
+    elif args.analyse_type == 'weight':
+        print(f"[AngelSlim] Save weight analyse graph to {args.save_path}")
+        os.makedirs(args.save_path, exist_ok=True)
+        assert os.path.exists(args.bf16_path), f"File {args.bf16_path} not exist"
+        assert os.path.exists(args.fp8_path), f"File {args.fp8_path} not exist"
+        bf16_path = args.bf16_path
+        fp8_path = args.fp8_path
+        layer_index = args.layer_index
+        draw_bf16_fp8_weight_fig(bf16_path=bf16_path, fp8_path=fp8_path, save_path=args.save_path, layer_index=layer_index)
+
+
 if __name__ == "__main__":
 
     global_parser = argparse.ArgumentParser(description="全局参数", add_help=True)
@@ -37,20 +54,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args(remaining_args)
     args.analyse_type = global_args.analyse_type
-    print(args)
-
-    if args.analyse_type == 'act':
-        os.makedirs(args.save_path, exist_ok=True)
-        assert os.path.exists(args.model_path), f"File {args.model_path} not exist"
-        print(f"[AngelSlim] Save all quant scale graph to {args.save_path}")
-        draw_fp8_scale_fig(args.model_path, args.save_path)
-    elif args.analyse_type == 'weight':
-        print(f"[AngelSlim] Save weight analyse graph to {args.save_path}")
-        os.makedirs(args.save_path, exist_ok=True)
-        assert os.path.exists(args.bf16_path), f"File {args.bf16_path} not exist"
-        assert os.path.exists(args.fp8_path), f"File {args.fp8_path} not exist"
-        bf16_path = args.bf16_path
-        fp8_path = args.fp8_path
-        layer_index = args.layer_index
-        draw_bf16_fp8_weight_fig(bf16_path=bf16_path, fp8_path=fp8_path, save_path=args.save_path, layer_index=layer_index)
+    print(f"Args:{args}")
+    quant_analyse(args)
 
