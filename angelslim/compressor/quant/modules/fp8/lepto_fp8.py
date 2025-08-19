b33c77af040e65146366dcd1688cbef2f9d3eab6
woodchen7 <woodchenwu@tencent.com>
2025-08-19 15:54:57 +0800

    format fix

--- angelslim/compressor/quant/modules/fp8/lepto_fp8.py
+++ angelslim/compressor/quant/modules/fp8/lepto_fp8.py
@@ -23,10 +23,10 @@ from ...core.quant_func import get_fp_maxval
 from ...modules.catcher import Catcher
 from .lepto_scale import AutoLayerScale
 
-__all__ = ["LEPTO_FP8"]
+__all__ = ["LeptpFP8"]
 
 
-class LEPTO_FP8:
+class LeptpFP8:
     def __init__(
         self,
         ptq_hook,
@@ -44,7 +44,7 @@ class LEPTO_FP8:
             model_arch_type(str, optional): model arch type.Default: None.
             low_memory(boll, optional): using low memory .Default: None.
         """
-        super(LEPTO_FP8, self).__init__()
+        super(LeptpFP8, self).__init__()
         self.ptq_hook = ptq_hook
         self.quant_model = model  # self.quant_model
         self.modal_type = self.quant_model.modal_type
